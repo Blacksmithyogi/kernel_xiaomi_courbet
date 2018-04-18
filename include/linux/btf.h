04530683a2d750dfa3b05eb5883d3f8b7a48b690
Martin KaFai Lau <kafai@fb.com>
2018-04-18 15:56:02 -0700

    UPSTREAM: bpf: btf: Add BPF_OBJ_GET_INFO_BY_FD support to BTF fd
    This patch adds BPF_OBJ_GET_INFO_BY_FD support to BTF fd.
    The original BTF data, which was used to create the BTF fd during
    the earlier BPF_BTF_LOAD call, will be returned.
    
    The userspace is expected to allocate buffer
    to info.info and the buffer size is set to info.info_len before
    calling BPF_OBJ_GET_INFO_BY_FD.
    
    The original BTF data is copied to the userspace buffer (info.info).
    Only upto the user's specified info.info_len will be copied.
    
    The original BTF data size is set to info.info_len.  The userspace
    needs to check if it is bigger than its allocated buffer size.
    If it is, the userspace should realloc with the kernel-returned
    info.info_len and call the BPF_OBJ_GET_INFO_BY_FD again.
    
    Change-Id: I313d083f69aa7fb3c0b7116d17e3187b586c239a
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Acked-by: Alexei Starovoitov <ast@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>
    Signed-off-by: Khensin1444 <wawa22arpk@gmail.com>

--- include/linux/btf.h
+++ include/linux/btf.h
@@ -23,6 +23,9 @@ int btf_get_info_by_fd(const struct btf *btf,
 void btf_put(struct btf *btf);
 int btf_new_fd(const union bpf_attr *attr);
 struct btf *btf_get_by_fd(int fd);
+int btf_get_info_by_fd(const struct btf *btf,
+		       const union bpf_attr *attr,
+		       union bpf_attr __user *uattr);
 /* Figure out the size of a type_id.  If type_id is a modifier
  * (e.g. const), it will be resolved to find out the type with size.
  *
