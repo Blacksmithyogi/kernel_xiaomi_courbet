e2d88a2272f3384c8de41fa2deedd1c8c4dd2863
Roman Gushchin <guroan@gmail.com>
2018-12-10 15:43:00 -0800

    UPSTREAM: bpf: pass struct btf pointer to the map_check_btf() callback
    If key_type or value_type are of non-trivial data types
    (e.g. structure or typedef), it's not possible to check them without
    the additional information, which can't be obtained without a pointer
    to the btf structure.
    
    So, let's pass btf pointer to the map_check_btf() callbacks.
    
    Change-Id: I95716060b450288d4ffcbe231d1cf5fdb530e292
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>
    Signed-off-by: Khensin1444 <wawa22arpk@gmail.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -460,6 +460,7 @@ static int bpf_obj_name_cpy(char *dst, const char *src)
 #define BPF_MAP_CREATE_LAST_FIELD map_ifindex
 #define BPF_MAP_CREATE_LAST_FIELD btf_value_id
 int map_check_no_btf(const struct bpf_map *map,
+		     const struct btf *btf,
 		     const struct btf_type *key_type,
 		     const struct btf_type *value_type)
 {
@@ -482,7 +483,7 @@ static int map_check_btf(const struct bpf_map *map, const struct btf *btf,
 		return -EINVAL;
 
 	if (map->ops->map_check_btf)
-		ret = map->ops->map_check_btf(map, key_type, value_type);
+		ret = map->ops->map_check_btf(map, btf, key_type, value_type);
 
 	return ret;
 }
