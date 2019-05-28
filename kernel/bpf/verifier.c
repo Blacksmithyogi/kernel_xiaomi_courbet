6810f661a25eeb717272809146d1fdaaebdc499c
brakmo <brakmo@fb.com>
2019-05-28 16:59:36 -0700

    UPSTREAM: bpf: cgroup inet skb programs can return 0 to 3
    Allows cgroup inet skb programs to return values in the range [0, 3].
    The second bit is used to deterine if congestion occurred and higher
    level protocol should decrease rate. E.g. TCP would call tcp_enter_cwr()
    
    The bpf_prog must set expected_attach_type to BPF_CGROUP_INET_EGRESS
    at load time if it uses the new return values (i.e. 2 or 3).
    
    The expected_attach_type is currently not enforced for
    BPF_PROG_TYPE_CGROUP_SKB.  e.g Meaning the current bpf_prog with
    expected_attach_type setting to BPF_CGROUP_INET_EGRESS can attach to
    BPF_CGROUP_INET_INGRESS.  Blindly enforcing expected_attach_type will
    break backward compatibility.
    
    This patch adds a enforce_expected_attach_type bit to only
    enforce the expected_attach_type when it uses the new
    return value.
    
    Change-Id: I5647151d48513d98c39fcf1d0381c31b5474e399
    Signed-off-by: Lawrence Brakmo <brakmo@fb.com>
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>
    Signed-off-by: Khensin1444 <wawa22arpk@gmail.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -5805,6 +5805,7 @@ static int check_ld_abs(struct bpf_verifier_env *env, struct bpf_insn *insn)
 
 static int check_return_code(struct bpf_verifier_env *env)
 {
+	struct tnum enforce_attach_type_range = tnum_unknown;
 	struct bpf_reg_state *reg;
 	struct tnum range = tnum_range(0, 1);
 
@@ -5814,6 +5815,10 @@ static int check_return_code(struct bpf_verifier_env *env)
 		    env->prog->expected_attach_type == BPF_CGROUP_UDP6_RECVMSG)
 			range = tnum_range(1, 1);
 	case BPF_PROG_TYPE_CGROUP_SKB:
+		if (env->prog->expected_attach_type == BPF_CGROUP_INET_EGRESS) {
+			range = tnum_range(0, 3);
+			enforce_attach_type_range = tnum_range(2, 3);
+		}
 	case BPF_PROG_TYPE_CGROUP_SOCK:
 	case BPF_PROG_TYPE_SOCK_OPS:
 	case BPF_PROG_TYPE_CGROUP_DEVICE:
@@ -5849,9 +5854,13 @@ static int check_return_code(struct bpf_verifier_env *env)
 		verbose(" should have been 0 or 1\n");
 		verbose(env, " should have been 0 or 1\n");
 		tnum_strn(tn_buf, sizeof(tn_buf), range);
-		verbose(env, " should have been in %s\n", tn_buf);
+		verbose(env, " should have been %s\n", tn_buf);
 		return -EINVAL;
 	}
+
+	if (!tnum_is_unknown(enforce_attach_type_range) &&
+	    tnum_in(enforce_attach_type_range, reg->var_off))
+		env->prog->enforce_expected_attach_type = 1;
 	return 0;
 }
 
