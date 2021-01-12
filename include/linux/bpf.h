546f3984276f076b527be19d40b94122c0e33277
Andrii Nakryiko <andrii@kernel.org>
2021-01-11 23:55:14 -0800

    UPSTREAM: bpf: Add bpf_patch_call_args prototype to include/linux/bpf.h
    [ Upstream commit a643bff752dcf72a07e1b2ab2f8587e4f51118be ]
    
    Add bpf_patch_call_args() prototype. This function is called from BPF verifier
    and only if CONFIG_BPF_JIT_ALWAYS_ON is not defined. This fixes compiler
    warning about missing prototype in some kernel configurations.
    
    Fixes: 1ea47e01ad6e ("bpf: add support for bpf_call to interpreter")
    Reported-by: kernel test robot <lkp@intel.com>
    Change-Id: Ic06a473f58bf111d74d23d5527f1b25be013db75
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Yonghong Song <yhs@fb.com>
    Link: https://lore.kernel.org/bpf/20210112075520.4103414-2-andrii@kernel.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>
    Signed-off-by: Khensin1444 <wawa22arpk@gmail.com>

--- include/linux/bpf.h
+++ include/linux/bpf.h
@@ -716,7 +716,10 @@ static inline void bpf_long_memcpy(void *dst, const void *src, u32 size)
 /* verify correctness of eBPF program */
 int bpf_check(struct bpf_prog **fp, union bpf_attr *attr,
 	      union bpf_attr __user *uattr);
+
+#ifndef CONFIG_BPF_JIT_ALWAYS_ON
 void bpf_patch_call_args(struct bpf_insn *insn, u32 stack_depth);
+#endif
 
 /* Map specifics */
 struct net_device  *__dev_map_lookup_elem(struct bpf_map *map, u32 key);
